df622ac260eea71382e20922cbab72523bd697d4
Anderson Vieira Machado <andersonvm1@gmail.com>
2024-03-24 01:39:21 -0300

    first commit

---
+++ start_node_exporter_rockylinux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install Node Exporter
+
+yum update -y
+yum install -y wget tar net-tools
+wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
+tar xvfz node_exporter-1.7.0.linux-amd64.tar.gz
+mv node_exporter-1.7.0.linux-amd64/node_exporter /usr/local/bin/
+rm -rf node_exporter-1.7.0.linux-amd64.tar.gz
+
+# Start Node Exporter
+/usr/local/bin/node_exporter &
